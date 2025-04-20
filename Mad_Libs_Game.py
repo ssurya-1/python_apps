628eeb665586fe2cdc61ce1ae8f14ea2574c8538
ssurya <suryasaravanan2020@gmail.com>
2025-04-20 12:37:46 +1000

    Adding final game

---
+++ Mad_Libs_Game.py
@@ -0,0 +1,22 @@
+
+print("It was a [Adjective] cold November day. I woke up to the [Another Adjective] smell of [Food] cooking in the "
+      "[Room] downstairs.")
+print(" I [Verb(Past Tense)] down the stairs to see if I could [Verb] with the lunch.")
+print("My mum told me to get [Noun] and [Another Verb] it so we could eat it as well.")
+
+Adjective1 = input( "Enter in Adjective1: ")
+Adjective2 = input( "Enter in another Adjective: ")
+Food = input("Enter in a type of food: ")
+Room = input("Enter in a room: ")
+verb = input("Enter a verb (past tense): ")
+Verb2 = input("Enter in a verb: ")
+noun = input("Enter in a noun: ")
+verb3 = input("Enter in another verb: ")
+
+
+print("It was a " + Adjective1 + " cold November day. I woke up to the " + Adjective2+  " smell of " + Food +
+      " cooking in the " + Room + " downstairs. ")
+print(" I " + verb + " down the stairs to see if I could " + Verb2 + " with the lunch.")
+print("My mum told me to get " + noun + " and " + verb3 + " it so we could eat it as well.")
+
+
